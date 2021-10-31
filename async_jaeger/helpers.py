c61800c60baa4d54255a9d8ed2ab541cd5a76b6a
Alexander Vasin <hi@alvass.in>
2021-10-31 22:37:37 +0300

    wip

--- async_jaeger/helpers.py
+++ async_jaeger/helpers.py
@@ -10,8 +10,14 @@ def encode_id(value: int, length: int, encoding = 'big') -> str:
     return value.to_bytes(length, encoding).hex()
 
 
-def encode_trace_id(trace_id):
+def encode_trace_id(trace_id: int) -> str:
+    if trace_id.bit_length() <= 64:
+        return encode_id(trace_id, 8)
     return encode_id(trace_id, 16)
+
+
+def decode_id(id_hex: str) -> int:
+    return int.from_bytes(bytes.fromhex(id_hex), 'big')
 
 
 def encode_span_id(span_id):
