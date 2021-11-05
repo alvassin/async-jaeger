efc1ab9a550b4f0b5e746c95973767d83d8df965
Alexander Vasin <hi@alvass.in>
2021-11-05 19:36:27 +0300

    Cosmetic

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2016 Uber Technologies, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import mock
 import random
 import socket
@@ -21,7 +7,7 @@ import tornado.httputil
 
 from opentracing import Format, child_of, follows_from
 from opentracing.ext import tags as ext_tags
-from async_jaeger import ConstSampler, SpanContext, Tracer
+from async_jaeger import ConstSampler, Tracer
 from async_jaeger import constants as c
 
 
